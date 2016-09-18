481280f94fc5cc58a1bf2a8c153e4d6f276a6ea3
ganeshvasekar <ganesh020619910@gmail.com>
2016-09-18 15:25:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
 });
 
 app.get('/',function(req, res){ res.sendFile(path.join(__dirname,'ui', 'page1.html'));
