feed88f2bc1a809f9bf7c9dc0e94b2010207d16a
ganeshvasekar <ganesh020619910@gmail.com>
2016-09-19 22:34:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.get('/page1',function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
 });
 
+app.get('/page2',function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'page2.html'));
+});
+
+app.get('/ganesh',function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'ganesh.html'));
+});
+
 app.get('/',function(req, res){ res.sendFile(path.join(__dirname,'ui', 'page1.html'));
     
 });
