5571d4044f968ae9d8e59ea570ff7de28b650085
ganeshvasekar <ganesh020619910@gmail.com>
2016-09-22 22:15:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,12 @@
 console.log('Loaded!');
 alert('hi,we are here to hack your system');
+// move image 
+var img = document.getElementById('g');
+var marginLeft=0;
+function moveRight(){
+    margineLeft = marginLeft+10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = fuction () {
+     var interval = setInterval(moveRight, 50);
+}
