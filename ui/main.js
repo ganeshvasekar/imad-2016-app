652a5f360b9db9ba58edf14d6b1f48d564c319f4
Ganesh Vasekar <ganeshvasekar@users.noreply.github.com>
2016-10-31 18:29:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,12 @@ img.onclick = function () {
 function myFunction() {
     document.getElementById("myForm").reset();
 }
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.2),
+    zoom: 10
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+src="https://maps.googleapis.com/maps/api/js?callback=myMap";
