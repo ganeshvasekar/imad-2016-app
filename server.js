0f70f15e008b80887988b58e2abc8c5b6b0a654a
Ganesh Vasekar <ganeshvasekar@users.noreply.github.com>
2016-11-05 21:05:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,8 +48,15 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/g.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'g.png'));
 });
-
-
+app.get('/ui/1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '1.jpg'));
+});
+app.get('/ui/2.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '2.jpg'));
+});
+app.get('/ui/3.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '3.jpg'));
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
