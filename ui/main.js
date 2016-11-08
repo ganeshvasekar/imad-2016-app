79a4d3b0702eb4d0b9e031106ed7c5b1cf3ac106
Ganesh Vasekar <ganeshvasekar@users.noreply.github.com>
2016-11-08 16:26:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,9 +9,10 @@
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
-                  submit.value = 'Sucess!';
+                  consol.log('user logged in');
+                  alert('logged in successfully');
               } else if (request.status === 403) {
-                  submit.value = 'Invalid credentials. Try again?';
+                   alert('Invalid credentials. Try again?');
               } else if (request.status === 500) {
                   alert('Something went wrong on the server');
                   submit.value = 'Login';
@@ -29,7 +30,7 @@
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
-        request.open('POST', '/login', true);
+        request.open('POST', 'http://ganeshvasekar.imad.hasura-app.io/login', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
         submit.value = 'Logging in...';
