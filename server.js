e989f691eca44f1d861574d9b71aa48a4096006d
Ganesh Vasekar <ganeshvasekar@users.noreply.github.com>
2016-11-08 13:07:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createTemplate (data) {
     var title = data.title;
@@ -74,6 +75,23 @@ function hash (input, salt) {
 app.get('/hash/:input', function(req, res) {
    var hashedString = hash(req.params.input, 'this-is-some-random-string');
    res.send(hashedString);
+});
+
+app.post('/create-user', function (req, res) {
+   // username, password
+   // {"username": "tanmai", "password": "password"}
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
 });
 
 app.get('/page1',function (req, res) {
