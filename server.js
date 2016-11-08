96b82ef680ed135c399159137093d1ae497a41eb
Ganesh Vasekar <ganeshvasekar@users.noreply.github.com>
2016-11-08 11:57:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,6 +71,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+function hash (input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
+});
+
 app.get('/page1',function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
 });
