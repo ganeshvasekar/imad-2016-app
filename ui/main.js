1ed43f5b85ea976e56e36dfd2dc212de2a4d4185
Ganesh Vasekar <ganeshvasekar@users.noreply.github.com>
2016-11-21 10:41:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -83,7 +83,7 @@ function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
     loginArea.innerHTML = `
         <h3> Hi <i>${username}</i></h3>
-        <a href="/signout">signout</a>
+        <a href="/Logout">Logout</a>
     `;
 }
 
