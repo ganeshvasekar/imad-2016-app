0597ee057105d2bbf5f1f2a8c91ae110ddbdb9f9
Ganesh Vasekar <ganeshvasekar@users.noreply.github.com>
2016-11-21 09:39:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -135,3 +135,25 @@ loadLogin();
 
 // Now this is something that we could have directly done on the server-side using templating too!
 loadArticles();
+
+ $(document).ready(function(){
+                $('#i-nav').click(function(){
+                   $('ul').toggleClass('show');
+                });
+                
+            });
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 4000); // Change image every 4 seconds
+}
