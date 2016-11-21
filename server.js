885fe37aef7f033659dd6495da255a33ed79dd4f
Ganesh Vasekar <ganeshvasekar@users.noreply.github.com>
2016-11-21 09:31:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -262,7 +262,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-app.get('/ui/main.js', function (req, res) {
+app.get('article.js', function (req, res) {
   res.sendFile(path.join(__dirname,  'article.js'));
 });
 
